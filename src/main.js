f9f7a9a1b210aac258ca14e6a1bdc878784db5cc
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2020-12-08 18:58:58 +0300

    initialize firebase

--- src/main.js
+++ src/main.js
@@ -6,13 +6,32 @@ import store from "./store";
 import messagePlugin from "@/utils/message.plugin";
 import "./registerServiceWorker";
 
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/database";
+
 Vue.config.productionTip = false;
 
 Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+firebase.initializeApp({
+  apiKey: "AIzaSyDV2MLZKQxoDAC3vPEdPMFroJZWYkqDoqg",
+  authDomain: "vue-crm-dd090.firebaseapp.com",
+  projectId: "vue-crm-dd090",
+  storageBucket: "vue-crm-dd090.appspot.com",
+  messagingSenderId: "805247621332",
+  appId: "1:805247621332:web:81bf6f915df2ff82fac341"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
