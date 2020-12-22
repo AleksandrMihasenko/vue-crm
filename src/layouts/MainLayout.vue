c13261d5335728834d1a3a6cbda05542a3585f93
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2020-12-22 20:11:07 +0300

    add homepage refreshing

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="app-main-layout">
-    <Navbar v-on:click="isOpen = !isOpen"></Navbar>
-    <Sidebar v-model="isOpen"></Sidebar>
-    <main class="app-content" v-bind:class="{ full: !isOpen }">
-      <div class="app-page">
-        <router-view></router-view>
-      </div>
-    </main>
+  <Loader v-if="loading"></Loader>
+  <div v-else>
+    <div class="app-main-layout">
+      <Navbar v-on:click="isOpen = !isOpen"></Navbar>
+      <Sidebar v-model="isOpen"></Sidebar>
+      <main class="app-content" v-bind:class="{ full: !isOpen }">
+        <div class="app-page">
+          <router-view></router-view>
+        </div>
+      </main>
 
-    <div class="fixed-action-btn">
-      <router-link class="btn-floating btn-large blue" to="/record">
-        <i class="large material-icons">add</i>
-      </router-link>
+      <div class="fixed-action-btn">
+        <router-link class="btn-floating btn-large blue" to="/record">
+          <i class="large material-icons">add</i>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -23,12 +26,14 @@ import Sidebar from "@/components/app/Sidebar";
 export default {
   name: "main-layout",
   data: () => ({
-    isOpen: true
+    isOpen: true,
+    loading: true
   }),
   async mounted() {
     if (!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch('fetchInfo');
     }
+    this.loading = false;
   },
   components: {
     Navbar,
