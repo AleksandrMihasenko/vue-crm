98e16b485a34ac400d99d2ec8e1ed77032f3f3e3
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2021-01-07 15:39:48 +0300

    add app title

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Vuelidate from "vuelidate";
 import Paginate from "vuejs-paginate";
+import VueMeta from 'vue-meta';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import messagePlugin from "@/utils/message.plugin";
+import titlePlugin from "@/utils/title.plugin";
 import Loader from "@/components/app/Loader";
 import "./registerServiceWorker";
 import firebase from "firebase";
@@ -17,7 +19,9 @@ import tooltipDirective from "@/directives/tooltip.directive";
 Vue.config.productionTip = false;
 
 Vue.use(messagePlugin);
+Vue.use(titlePlugin);
 Vue.use(Vuelidate);
+Vue.use(VueMeta);
 Vue.filter("currency", currencyFilter);
 Vue.filter("localize", localizeFilter);
 Vue.directive("tooltip", tooltipDirective);
